ad9631b76b0650418288cb97955897bda8f98a62
shino72 <a36243845@gmail.com>
2023-02-06 23:53:05 +0900

    MainActivity 완성

---
+++ app/src/main/java/com/shino72/fractionking/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shino72.fractionking.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shino72.fractionking.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
